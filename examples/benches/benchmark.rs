5b411f051b9b1e31b8f88d7c6375b3860585fe2b
Mohsen Zainalpour <zainalpour@gmail.com>
2026-03-04 23:18:13 -0500

    style: fix formatting in benchmark.rs

--- examples/benches/benchmark.rs
+++ examples/benches/benchmark.rs
@@ -24,8 +24,7 @@ fn benchmark_run_client(c: &mut Criterion) {
     group.bench_function("run_client", |b| {
         b.iter(|| {
             let runtime = tokio::runtime::Runtime::new().expect("tokio runtime");
-            let _ =
-                runtime.block_on(async { black_box(run_client("http://0.0.0.0:50051")).await });
+            let _ = runtime.block_on(async { black_box(run_client("http://0.0.0.0:50051")).await });
         })
     });
 
